f24f364969f6744bdc6418bc623c73de67c80459
Sawyer Watts <sawyer.watts@gmail.com>
2025-08-26 19:43:12 -0700

    mock out behaviors and ecs parts

--- demos/EquipmentScreen/Driver.cs
+++ demos/EquipmentScreen/Driver.cs
@@ -1,48 +1,55 @@
-using System.ComponentModel.DataAnnotations;
-
-using Microsoft.Extensions.Options;
-
 using Spectre.Console;
 
 namespace EquipmentScreen;
 
+// TODO: this demo
+//      have a screen to perform various ops (ctl for many chars?)
+//          view stats (raw and w/ modifiers)
+//              health
+//              defense
+//              attack
+//          un/equip dagger, scimitar, zweihander
+//          un/equip none, light, medium, or heavy armor
+//          un/equip some rings
+//              small being's ring
+//              red tearstone
+//              ring of blades
+//          inflict variable damage (respect armor)
+//          heal variable health instantly or gradually
+//          apply poison
+//              have variable buildup and decay?
+//          tick poison
+//          cure poison
+//      entities
+//          character
+//          weapons
+//          armors
+//          rings
+//      components
+//          base stats: health, def, attack, weight
+//          statuses
+//          stat modifier, with additive+overrideBool and multiplicative fields per stat
+//      systems
+//          un/equip equipment
+//          inflict variable damage
+//          instant or gradual variable healing
+//          apply/cure poison
+//          tick game on every (non-view stats) action
+//              poison
+//              gradual healing
+
 public class Driver
 {
-    private readonly Settings _settings;
-    private readonly ILogger<Driver> _logger;
-
-    public Driver(
-        IOptions<Settings> settings,
-        ILogger<Driver> logger)
-    {
-        _settings = settings.Value;
-        _logger = logger;
-    }
-
-    public Task RunAsync(
-        CancellationToken cancellationToken)
+    public Task RunAsync(CancellationToken cancellationToken)
     {
         // TODO: start coding here
         AnsiConsole.WriteLine("Start coding here");
         return Task.CompletedTask;
     }
 
-    public static void RegisterTo(
-        IHostApplicationBuilder builder)
+    public static void RegisterTo(IHostApplicationBuilder builder)
     {
         builder.Services.AddTransient<Driver>();
-        builder.Services.AddSingleton<IValidateOptions<Settings>, ValidateDriverSettings>();
-        builder.Services.AddOptions<Settings>()
-            .Bind(builder.Configuration.GetRequiredSection("Driver"))
-            .ValidateOnStart();
-    }
-
-    public class Settings
-    {
-        [Required]
-        public string Demo { get; set; } = "";
     }
 }
 
-[OptionsValidator]
-public partial class ValidateDriverSettings : IValidateOptions<Driver.Settings>;
